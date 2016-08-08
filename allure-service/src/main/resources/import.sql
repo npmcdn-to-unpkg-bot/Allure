c4efeea6a90823331fbcf713280613c2f8d39ca1
yang_shoulai <412837184@qq.com>
2016-08-08 14:41:11 +0800

    add maven support for allure web

--- allure-service/src/main/resources/import.sql
+++ allure-service/src/main/resources/import.sql
@@ -6,10 +6,3 @@ INSERT INTO sys_role (id, version, role, description) VALUES (2, 1, 'ROLE_USER',
 INSERT INTO sys_account_role (account, role) VALUES (1, 1);
 INSERT INTO sys_account_role (account, role) VALUES (1, 2);
 
-INSERT INTO t_tag (id, version, name) VALUES (1, 1, 'AA');
-INSERT INTO t_tag (id, version, name) VALUES (2, 1, 'BB');
-INSERT INTO t_tag (id, version, name) VALUES (3, 1, 'CC');
-INSERT INTO t_tag (id, version, name) VALUES (4, 1, 'DD');
-INSERT INTO t_tag (id, version, name) VALUES (5, 1, 'EE');
-INSERT INTO t_tag (id, version, name) VALUES (6, 1, 'FF');
-INSERT INTO t_tag (id, version, name) VALUES (7, 1, 'GG');
